3b3c7d15c13eda59768549e6a44051af8376f7b9
fak3mistak3 <mirolyubov.dev@gmail.com>
2023-11-18 14:57:41 +0300

    add removing messages flow

--- keyboards/settings.go
+++ keyboards/settings.go
@@ -5,8 +5,10 @@ import tgbotapi "github.com/go-telegram-bot-api/telegram-bot-api/v5"
 func SettingsMainReplyKeyboard() tgbotapi.InlineKeyboardMarkup {
 	keyboard := tgbotapi.NewInlineKeyboardMarkup(
 		tgbotapi.NewInlineKeyboardRow(
-			tgbotapi.NewInlineKeyboardButtonData("Next", "next,yes"),
-			tgbotapi.NewInlineKeyboardButtonData("Stop", "next,no"),
+			tgbotapi.NewInlineKeyboardButtonData("Данные", "data"),
+		),
+		tgbotapi.NewInlineKeyboardRow(
+			tgbotapi.NewInlineKeyboardButtonData("Выход", "exit"),
 		),
 	)
 
--- main.go
+++ main.go
@@ -6,8 +6,7 @@ import (
 	"data-miner/menu"
 	"data-miner/settings"
 	"log"
-
-	"github.com/davecgh/go-spew/spew"
+	"strings"
 
 	tgbotapi "github.com/go-telegram-bot-api/telegram-bot-api/v5"
 )
@@ -33,34 +32,78 @@ func main() {
 		if update.Message != nil {
 			log.Printf("[%s] %s", update.Message.From.UserName, update.Message.Text)
 		}
-		spew.Dump(update)
 		if update.CallbackQuery != nil {
+			callback := tgbotapi.NewCallback(update.CallbackQuery.ID, update.CallbackQuery.Data)
+			callback.Text = ""
+			tgbot.Request(callback)
 
-		} else {
+			callbackData := strings.Split(update.CallbackQuery.Data, ",")
 
+			switch callbackData[0] {
+			case "exit":
+				deleteMessageRequest := tgbotapi.NewDeleteMessage(update.CallbackQuery.Message.Chat.ID, update.CallbackQuery.Message.MessageID)
+				tgbot.Request(deleteMessageRequest)
+			case "data":
+
+			default:
+				continue
+			}
+		} else if update.Message.IsCommand() {
 			msg := tgbotapi.NewMessage(update.Message.From.ID, "test")
 
-			if update.Message.IsCommand() {
-				switch update.Message.Command() {
-				case "start":
-					ok, err := db.CreateNewUserOrCheckExist(update.Message.From)
-					if err != nil {
-						// handle db err
-					}
-					if ok {
-						msg.Text = "Hello"
-					} else {
-						msg.Text = "Hello again"
-					}
-				case "settings":
-					msg = settings.MainSettingsMessage(update.Message.From.ID)
-					msg.Text = "Settings"
-				default:
-					continue
+			switch update.Message.Command() {
+			case "start":
+				ok, err := db.CreateNewUserOrCheckExist(update.Message.From)
+				if err != nil {
+					// handle db err
 				}
+				if ok {
+					msg.Text = "Hello"
+				} else {
+					msg.Text = "Hello again"
+				}
+			case "settings":
+				msg = settings.MainSettingsMessage(update.Message.From.ID)
+				msg.Text = "Settings"
+			default:
+				continue
 			}
 			tgbot.Send(msg)
+			DeleteCurrentMessage(update)
+		} else {
+			DeleteCurrentMessage(update)
+
 		}
 
 	}
+}
+
+func DeletePrevMessage(update tgbotapi.Update) error {
+	tgbot := bot.GetBot()
+	var deleteMessageRequest tgbotapi.DeleteMessageConfig
+
+	if update.Message != nil {
+		deleteMessageRequest = tgbotapi.NewDeleteMessage(update.Message.Chat.ID, update.Message.MessageID-1)
+	}
+	if update.CallbackQuery != nil {
+		deleteMessageRequest = tgbotapi.NewDeleteMessage(update.CallbackQuery.Message.Chat.ID, update.CallbackQuery.Message.MessageID-1)
+	}
+	_, err := tgbot.Request(deleteMessageRequest)
+
+	return err
+}
+
+func DeleteCurrentMessage(update tgbotapi.Update) error {
+	tgbot := bot.GetBot()
+	var deleteMessageRequest tgbotapi.DeleteMessageConfig
+
+	if update.Message != nil {
+		deleteMessageRequest = tgbotapi.NewDeleteMessage(update.Message.Chat.ID, update.Message.MessageID)
+	}
+	if update.CallbackQuery != nil {
+		deleteMessageRequest = tgbotapi.NewDeleteMessage(update.CallbackQuery.Message.Chat.ID, update.CallbackQuery.Message.MessageID)
+	}
+	_, err := tgbot.Request(deleteMessageRequest)
+
+	return err
 }
